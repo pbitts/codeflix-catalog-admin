57054bd149a8f7df2abfeb44ed318beb53c852da
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-19 16:11:55 -0300

    implement activate deactivate __eq__ and turn into dataclass

--- category.py
+++ category.py
@@ -1,18 +1,47 @@
+from dataclasses import dataclass, field
 import uuid
 
-
+@dataclass
 class Category:
-    def __init__(self, name, id = "", description = "", is_active = True):
-        self.name = name
-        self.id = id or uuid.uuid4()
-        self.description = description
-        self.is_active = is_active
-
-        if len(self.name) > 255:
-            raise ValueError("name must be less than 256 characters")
+    name: str
+    description: str = ""
+    is_active: bool = True
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
     
+    def __post_init__(self):
+        self.validate()
+    
+    def validate(self):
+        if len(self.name) > 255:
+            raise ValueError("name cannot be longer than 255")
+
+        if not self.name:
+            raise ValueError("name cannot be empty")
+
     def __str__(self):
         return f"Category(name={self.name} - description={self.description} (is_active={self.is_active}))"
 
     def __repr__(self):
         return self.__str__()
+
+    def __eq__(self, other):
+        if not isinstance(other, Category):
+            return False
+        return self.id == other.id
+
+    def update(self, name = None, description = None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+
+        self.validate()
+
+    def activate(self):
+        self.is_active = True
+        self.validate()
+
+    def deactivate(self):
+        self.is_active = False
+        self.validate()
+    
