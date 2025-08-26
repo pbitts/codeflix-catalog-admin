12f2ee6ee7ea456bc5cd5d71429598f7b2b47055
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-26 11:15:03 -0300

    create genre integration test

---
+++ src/core/genre/tests/application/integration/test_create_genre.py
@@ -0,0 +1,98 @@
+import uuid
+import pytest
+
+from src.core.genre.infra.in_memory_genre_repository import InMemoryGenreRepository
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+from src.core.genre.domain.genre import Genre
+from src.core.genre.application.exceptions import InvalidGenre, RelatedCategoriesNotFound
+from src.core.genre.application.use_cases.create_genre import CreateGenre
+from src.core.category.domain.category import Category
+from src.core.category.domain.category_repository import CategoryRepository
+from src.core.genre.domain.genre_repository import GenreRepository
+
+
+@pytest.fixture
+def movie_category() -> Category:
+    return Category(name="Movie")
+
+@pytest.fixture
+def documentary_category() -> Category:
+    return Category(name="Documentary")
+    
+@pytest.fixture
+def category_repository(movie_category, documentary_category) -> CategoryRepository:
+    return InMemoryCategoryRepository(
+        categories=[movie_category, documentary_category]
+    )
+
+class TestCreateGenre:
+    def test_create_genre_with_associated_categories(
+        self,
+        movie_category,
+        documentary_category,
+        category_repository,
+    ):
+        genre_repository = InMemoryGenreRepository()
+        use_case = CreateGenre(
+            repository=genre_repository,
+            category_repository=category_repository
+        )
+        input = CreateGenre.Input(
+            name="Action",
+            category_ids={movie_category.id, documentary_category.id}
+        )
+        
+        output = use_case.execute(input)
+        
+        assert isinstance(output, CreateGenre.Output)
+        assert isinstance(output.id, uuid.UUID)
+        assert len(genre_repository.genres) == 1
+        genre = genre_repository.get_by_id(output.id)
+        assert genre.id == output.id
+        assert genre.name == "Action"
+        assert genre.is_active is True
+        assert genre.categories == {movie_category.id, documentary_category.id}
+    
+    def test_create_genre_with_inexistent_categories_raise_an_error(
+        self,
+        category_repository,
+    ):
+        genre_repository = InMemoryGenreRepository()
+        use_case = CreateGenre(
+            repository=genre_repository,
+            category_repository=category_repository
+        )
+        input = CreateGenre.Input(
+            name="Action",
+            category_ids={uuid.uuid4(), uuid.uuid4()}
+        )
+        
+        with pytest.raises(RelatedCategoriesNotFound) as exc_info:
+            use_case.execute(input)
+        
+        assert "Categories not found" in str(exc_info.value)
+        assert len(genre_repository.genres) == 0
+        
+    def test_create_genre_without_categories(
+        self,
+        category_repository,
+    ):
+        genre_repository = InMemoryGenreRepository()
+        use_case = CreateGenre(
+            repository=genre_repository,
+            category_repository=category_repository
+        )
+        input = CreateGenre.Input(
+            name="Action",
+        )
+        
+        output = use_case.execute(input)
+        
+        assert isinstance(output, CreateGenre.Output)
+        assert isinstance(output.id, uuid.UUID)
+        assert len(genre_repository.genres) == 1
+        genre = genre_repository.get_by_id(output.id)
+        assert genre.id == output.id
+        assert genre.name == "Action"
+        assert genre.is_active is True
+        assert genre.categories == set()
