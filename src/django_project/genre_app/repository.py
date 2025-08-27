920773219046c59f3d4ab50b1efa11440d10c8bb
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-26 21:34:28 -0300

    implements repository and tests for all operations

--- src/django_project/genre_app/repository.py
+++ src/django_project/genre_app/repository.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from django.db import transaction
 
 from src.core.genre.domain.genre import Genre
@@ -15,14 +16,42 @@ class DjangoORMGenreRepository(GenreRepository):
             )
             genre_model.categories.set(genre.categories)
     
-    def get_by_id(self, genre_id: str) -> Genre:
-        pass
+    def get_by_id(self, id: UUID) -> Genre:
+        try:
+            genre_model = GenreModel.objects.get(id=id)
+        except GenreModel.DoesNotExist:
+            return None
+        return Genre(
+            id=genre_model.id,
+            name=genre_model.name,
+            is_active=genre_model.is_active,
+            categories=[cat.id for cat in genre_model.categories.all()]
+        )
     
     def list(self) -> list[Genre]:
-        pass
+        return [
+            Genre(
+                id=genre_model.id,
+                name=genre_model.name,
+                is_active=genre_model.is_active,
+                categories=[cat.id for cat in genre_model.categories.all()]
+            )
+            for genre_model in GenreModel.objects.all()
+        ]
     
-    def update(self, genre: Genre):
-        pass
+    def update(self, genre: Genre) -> None:
+        try:
+            genre_model = GenreModel.objects.get(id=genre.id)
+        except GenreModel.DoesNotExist:
+            return None
+        
+        with transaction.atomic():
+            GenreModel.objects.filter(id=genre.id).update(
+                name=genre.name,
+                is_active=genre.is_active,
+            )
+            # Sets a final state to the categories relation
+            genre_model.categories.set(genre.categories)
     
-    def delete(self, genre_id: str):
-        pass
+    def delete(self, id: UUID) -> None:
+        GenreModel.objects.filter(id=id).delete()
--- src/django_project/genre_app/tests/test_repository.py
+++ src/django_project/genre_app/tests/test_repository.py
@@ -1,3 +1,4 @@
+import uuid
 import pytest
 
 from src.core.category.domain.category import Category
@@ -41,3 +42,116 @@ class TestSave:
         related_category = genre_model.categories.first()
         assert related_category.id == category.id
         assert genre_model.name == "Action"
+
+
+@pytest.mark.django_db
+class TestList:
+    def test_list_genres(self):
+        repository = DjangoORMGenreRepository()
+        genre1 = Genre(name="Action")
+        genre2 = Genre(name="Horror", is_active=False)
+        
+        repository.save(genre1)
+        repository.save(genre2)
+        
+        genres = repository.list()
+        
+        assert len(genres) == 2
+        assert genres[0].id == genre1.id
+        assert genres[0].name == "Action"
+        assert genres[0].is_active is True
+        
+        assert genres[1].id == genre2.id
+        assert genres[1].name == "Horror"
+        assert genres[1].is_active is False
+        
+        
+@pytest.mark.django_db
+class TestGetById:
+    def test_get_genre_by_id(self):
+        repository = DjangoORMGenreRepository()
+        genre = Genre(name="Action")
+        
+        repository.save(genre)
+        
+        found_genre = repository.get_by_id(genre.id)
+        
+        assert found_genre is not None
+        assert found_genre.id == genre.id
+        assert found_genre.name == "Action"
+        assert found_genre.is_active is True
+    
+    def test_get_genre_by_id_not_found(self):
+        repository = DjangoORMGenreRepository()
+        
+        found_genre = repository.get_by_id(uuid.uuid4())
+        
+        assert found_genre is None
+        
+
+@pytest.mark.django_db
+class TestUpdate:
+    def test_update_genre(self):
+        repository = DjangoORMGenreRepository()
+        category_repository = DjangoORMCategoryRepository()
+        
+        category1 = Category(name='Movie')
+        category2 = Category(name='Documentary')
+        category_repository.save(category1)
+        category_repository.save(category2)
+        
+        genre = Genre(name="Action")
+        genre.add_category(category1.id)
+        repository.save(genre)
+        
+        # Update genre details
+        genre.name = "Action Updated"
+        genre.is_active = False
+        genre.add_category(category2.id)
+        
+        repository.update(genre)
+        
+        updated_genre_model = GenreModel.objects.get(id=genre.id)
+        
+        assert updated_genre_model.name == "Action Updated"
+        assert updated_genre_model.is_active is False
+        related_categories = list(updated_genre_model.categories.all())
+        assert len(related_categories) == 2
+        assert category1.id in [cat.id for cat in related_categories]
+        assert category2.id in [cat.id for cat in related_categories]
+    
+    def test_update_genre_not_found(self):
+        repository = DjangoORMGenreRepository()
+        
+        genre = Genre(name="Non-existent")
+        
+        result = repository.update(genre)
+        
+        assert result is None
+        
+@pytest.mark.django_db
+class TestDelete:
+    def test_delete_genre(self):
+        repository = DjangoORMGenreRepository()
+        genre = Genre(name="Action")
+        
+        repository.save(genre)
+        
+        assert GenreModel.objects.count() == 1
+        
+        repository.delete(genre.id)
+        
+        assert GenreModel.objects.count() == 0
+    
+    def test_delete_genre_not_found(self):
+        repository = DjangoORMGenreRepository()
+        repository = DjangoORMGenreRepository()
+        genre = Genre(name="Action")
+        
+        repository.save(genre)
+        assert GenreModel.objects.count() == 1
+        
+        repository.delete(uuid.uuid4())
+        
+        assert GenreModel.objects.count() == 1
+        assert GenreModel.objects.get(id=genre.id) is not None
