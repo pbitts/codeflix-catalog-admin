b9f5d23f03cea154171f488b39cc13b8c0fe685d
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-28 14:33:10 -0300

    list castmember integration tests

---
+++ src/core/castmember/tests/application/integration/test_list_castmember.py
@@ -0,0 +1,58 @@
+
+
+from src.core.castmember.application.use_cases.list_castmember import ListCastMember, CastMemberOutput
+from src.core.castmember.domain.castmember import CastMember
+from src.core.castmember.infra.in_memory_castmember_repository import InMemoryCastMemberRepository
+
+
+class TestListCastMember:
+    def test_list_castmember(
+        self,
+    ):
+        
+        castmember_repository = InMemoryCastMemberRepository()
+        actor = CastMember(
+            name="Alvin",
+            type='ACTOR'
+        )
+        director = CastMember(
+            name="Ravier",
+            type='DIRECTOR'
+        )
+        castmember_repository.save(actor)
+        castmember_repository.save(director)
+        
+        use_case = ListCastMember(
+            repository=castmember_repository
+        )
+        
+        output = use_case.execute(ListCastMember.Input())
+        
+        assert len(output.data) == 2
+        assert output == ListCastMember.Output(
+            data=[
+                CastMemberOutput(
+                    id=actor.id,
+                    name=actor.name,
+                    type=actor.type
+                ),
+                CastMemberOutput(
+                    id=director.id,
+                    name=director.name,
+                    type=director.type
+                )
+            ]
+        )
+    
+    def test_list_empty_castmember_list(
+        self,
+    ):
+        castmember_repository = InMemoryCastMemberRepository()
+        use_case = ListCastMember(
+            repository=castmember_repository
+        )
+        
+        output = use_case.execute(ListCastMember.Input())
+        
+        assert len(output.data) == 0
+        assert output == ListCastMember.Output(data=[])
