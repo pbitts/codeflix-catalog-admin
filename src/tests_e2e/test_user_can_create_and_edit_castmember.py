c4cecf581ac5920f7c787597d63671aaf16cb321
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-29 19:47:21 -0300

    tests e2e castmemebrs

---
+++ src/tests_e2e/test_user_can_create_and_edit_castmember.py
@@ -0,0 +1,103 @@
+import pytest
+
+from rest_framework.test import APIClient
+
+@pytest.mark.django_db
+class TestCreateAndEditCastMember:
+    def test_user_can_Create_and_edit_category(self) -> None:
+        api_client = APIClient()
+        
+        # Verifies empty list
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.data == {"data": []}
+        
+        # Creates a new castmember
+        create_response = api_client.post(
+            "/api/cast_members/", 
+            {
+                "name": "Monica",
+                "type": "DIRECTOR"
+                },
+            )
+        assert create_response.status_code == 201
+        created_castmember_id = create_response.data["id"]
+        
+        # Verifies created category is listed
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.status_code == 200
+        assert list_response.data == {
+            "data": [
+                {
+                    "id": created_castmember_id,
+                    "name": "Monica",
+                    "type": "DIRECTOR"
+                }
+            ]
+        }
+        
+        # Updates the created castmember
+        update_response = api_client.put(
+            f"/api/cast_members/{created_castmember_id}/", 
+            {
+                "name": "Monica Updated",
+                "type": 'ACTOR'
+            },
+        )
+        assert update_response.status_code == 204
+        assert update_response.data is None
+        
+        # Verifies updated category is listed
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.data == {
+            "data": [
+                {
+                    "id": created_castmember_id,
+                "name": "Monica Updated",
+                "type": 'ACTOR'
+                }
+            ]
+        }
+    
+
+@pytest.mark.django_db
+class TestCreateAndDeleteCastMember:
+    def test_user_can_create_and_delete_castmember(self) -> None:
+        api_client = APIClient()
+        
+        # Verifies empty list
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.data == {"data": []}
+        
+        # Creates a new castmember
+        create_response = api_client.post(
+            "/api/cast_members/", 
+            {
+                "name": "Magali",
+                "type": "ACTOR"
+                },
+            )
+        assert create_response.status_code == 201
+        created_castmember_id = create_response.data["id"]
+        
+        # Verifies created castmember is listed
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.status_code == 200
+        assert list_response.data == {
+            "data": [
+                {
+                    "id": created_castmember_id,
+                    "name": "Magali",
+                "type": "ACTOR"
+                }
+            ]
+        }
+        
+        # Deletes the created castmember
+        delete_response = api_client.delete(f"/api/cast_members/{created_castmember_id}/")
+        assert delete_response.status_code == 204
+        assert delete_response.data is None
+        
+        # Verifies empty list again
+        list_response = api_client.get("/api/cast_members/")
+        assert list_response.data == {"data": []}
+        
