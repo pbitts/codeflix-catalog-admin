63cd66044ca311852f2403b9c3c6728dac2fc46a
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-31 17:36:15 -0300

    category model mapper

--- src/django_project/category_app/repository.py
+++ src/django_project/category_app/repository.py
@@ -12,22 +12,25 @@ class DjangoORMCategoryRepository(CategoryRepository):
         self.category_model = category_model
         
     def save(self, category: Category) -> None:
-        self.category_model.objects.create(
-            id=category.id,
-            name=category.name,
-            description=category.description,
-            is_active=category.is_active
-        )
+        category_orm = CategoryModelMapper.to_model(category)
+        category_orm.save()
+        # self.category_model.objects.create(
+        #     id=category.id,
+        #     name=category.name,
+        #     description=category.description,
+        #     is_active=category.is_active
+        # )
     
     def get_by_id(self, id: UUID) -> Category | None:
         try:
-            category = self.category_model.objects.get(id=id)
-            return Category(
-                id=category.id,
-                name=category.name,
-                description=category.description,
-                is_active=category.is_active
-            )
+            category_model = self.category_model.objects.get(id=id)
+            return CategoryModelMapper.to_entity(category_model)
+            # return Category(
+            #     id=category.id,
+            #     name=category.name,
+            #     description=category.description,
+            #     is_active=category.is_active
+            # )
         except self.category_model.DoesNotExist:
             return None
         
@@ -35,15 +38,19 @@ class DjangoORMCategoryRepository(CategoryRepository):
         self.category_model.objects.filter(id=id).delete()
     
     def list(self) -> list[Category]:
-         return [
-             Category(
-                 id=category.id,
-                 name=category.name,
-                 description=category.description,
-                 is_active=category.is_active
-             )
-             for category in self.category_model.objects.all()
-         ]
+        return [
+            CategoryModelMapper.to_entity(category_model)
+            for category_model in self.category_model.objects.all()
+        ]
+        #  return [
+        #      Category(
+        #          id=category.id,
+        #          name=category.name,
+        #          description=category.description,
+        #          is_active=category.is_active
+        #      )
+        #      for category in self.category_model.objects.all()
+        #  ]
     
     def update(self, category: Category) -> None:
         self.category_model.objects.filter(pk=category.id).update(
@@ -51,3 +58,22 @@ class DjangoORMCategoryRepository(CategoryRepository):
             description=category.description,
             is_active=category.is_active
         )
+
+class CategoryModelMapper:
+    @staticmethod
+    def to_model(category: Category) -> CategoryModel:
+        return CategoryModel(
+            id=category.id,
+            name=category.name,
+            description=category.description,
+            is_active=category.is_active
+        )
+    
+    @staticmethod
+    def to_entity(category_orm: CategoryModel) -> Category:
+            return Category(
+                id=category_orm.id,
+                name=category_orm.name,
+                description=category_orm.description,
+                is_active=category_orm.is_active
+            )
