6003c51ef315936dc4b1d4b55a8631d331b0a147
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-08-31 17:46:39 -0300

    castmember  model mapper

--- src/django_project/castmember_app/repository.py
+++ src/django_project/castmember_app/repository.py
@@ -8,33 +8,40 @@ from src.django_project.castmember_app.models import CastMember as CastMemberMod
 class DjangoORMCastMemberRepository(CastMemberRepository):
     
     def save(self, castmember: CastMember):
-        with transaction.atomic():
-            castmember_model = CastMemberModel.objects.create(
-                id=castmember.id,
-                name=castmember.name,
-                type=castmember.type,
-            )
+        castmember_orm = CastMemberModelMapper.to_model(castmember)
+        castmember_orm.save()
+        # with transaction.atomic():
+        #     castmember_model = CastMemberModel.objects.create(
+        #         id=castmember.id,
+        #         name=castmember.name,
+        #         type=castmember.type,
+        #     )
     
     def get_by_id(self, id: UUID) -> CastMember:
         try:
             castmember_model = CastMemberModel.objects.get(id=id)
+            return CastMemberModelMapper.to_entity(castmember_model)
         except CastMemberModel.DoesNotExist:
             return None
-        return CastMember(
-            id=castmember_model.id,
-            name=castmember_model.name,
-            type=castmember_model.type
-        )
+        # return CastMember(
+        #     id=castmember_model.id,
+        #     name=castmember_model.name,
+        #     type=castmember_model.type
+        # )
     
     def list(self) -> list[CastMember]:
         return [
-            CastMember(
-                id=castmember_model.id,
-                name=castmember_model.name,
-                type=castmember_model.type
-            )
+            CastMemberModelMapper.to_entity(castmember_model)
             for castmember_model in CastMemberModel.objects.all()
         ]
+        # return [
+        #     CastMember(
+        #         id=castmember_model.id,
+        #         name=castmember_model.name,
+        #         type=castmember_model.type
+        #     )
+        #     for castmember_model in CastMemberModel.objects.all()
+        # ]
     
     def update(self, castmember: CastMember) -> None:
         try:
@@ -50,3 +57,20 @@ class DjangoORMCastMemberRepository(CastMemberRepository):
     
     def delete(self, id: UUID) -> None:
         CastMemberModel.objects.filter(id=id).delete()
+        
+class CastMemberModelMapper:
+    @staticmethod
+    def to_model(castmember: CastMember) -> CastMemberModel:
+        return CastMemberModel(
+            id=castmember.id,
+            name=castmember.name,
+            type=castmember.type,
+            )
+    
+    @staticmethod
+    def to_entity(castmember_model: CastMemberModel) -> CastMember:
+            return CastMember(
+            id=castmember_model.id,
+            name=castmember_model.name,
+            type=castmember_model.type
+        )
